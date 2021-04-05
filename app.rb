2bf6797d4391c0bc671d6698b5fc439df8cc02a7
Iga Jedras <iga@MacBook-Pro-Iga.local>
2021-04-05 10:57:32 +0200

    podstrony

--- app.rb
+++ app.rb
@@ -7,3 +7,15 @@ end
 get '/o-mnie' do
   File.read(File.join('pages', 'o-mnie.html'))
 end
+
+get '/moje-psy' do
+  File.read(File.join('pages', 'moje-psy.html'))
+end
+
+get '/szczenieta' do
+  File.read(File.join('pages', 'szczenieta.html'))
+end
+
+get '/kontakt' do
+  File.read(File.join('pages', 'kontakt.html'))
+end
