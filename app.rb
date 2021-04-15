957e3a3a39f7af4df4496b954398b4bf55ed6327
Iga Jedras <iga@MacBook-Pro-Iga.local>
2021-04-15 22:11:10 +0200

    aktualizacja

--- app.rb
+++ app.rb
@@ -19,3 +19,31 @@ end
 get '/kontakt' do
   File.read(File.join('pages', 'kontakt.html'))
 end
+
+get '/janis' do
+  File.read(File.join('pages', 'janis.html'))
+end
+
+get '/samba' do
+  File.read(File.join('pages', 'samba.html'))
+end
+
+get '/trek' do
+  File.read(File.join('pages', 'trek.html'))
+end
+
+get '/flesz' do
+  File.read(File.join('pages', 'flesz.html'))
+end
+
+get '/potomstwo-trek' do
+  File.read(File.join('pages', 'potomstwo-trek.html'))
+end
+
+get '/miot2012' do
+  File.read(File.join('pages', 'miot2012.html'))
+end
+
+get '/miot2015' do
+  File.read(File.join('pages', 'miot2015.html'))
+end
